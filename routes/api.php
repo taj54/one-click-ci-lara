ac3bd9e074cce36c3d83b95ec04d6741a143b8bb
Taj <tajulislamj200@gmail.com>
2025-06-11 02:39:50 +0530

    api Route upload-detect is completed

--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,6 @@
 use App\Http\Controllers\Api\APICIMigrationController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::prefix('migration')->group(function () {
     // New endpoint for uploading and detecting CI version
